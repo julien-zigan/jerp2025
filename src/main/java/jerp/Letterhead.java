9b93c6236a08864f289f88b7263739c8949ec95e
Julien <zigan.julien@gmail.com>
2025-01-09 15:38:38 +0100

    add letterhead graphics to test ressources

--- src/main/java/jerp/Letterhead.java
+++ src/main/java/jerp/Letterhead.java
@@ -1,5 +1,18 @@
 package jerp;
 
-public class Letterhead {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class Letterhead {
+    private final BufferedImage content;
+
+    public Letterhead(String imagePath) throws IOException {
+        content = ImageIO.read(new File(imagePath));
+    }
+
+    public BufferedImage getContent() {
+        return content;
+    }
 }
