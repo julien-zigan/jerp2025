a9e89d3130f0c2acb2545459b19c85a3e11d9337
Julien <zigan.julien@gmail.com>
2025-01-12 22:52:29 +0100

    add requirePNGorJPEG() to check constructor input

--- src/main/java/jerp/Letterhead.java
+++ src/main/java/jerp/Letterhead.java
@@ -4,15 +4,31 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 public class Letterhead {
     private final BufferedImage content;
 
-    public Letterhead(String imagePath) throws IOException {
-        content = ImageIO.read(new File(imagePath));
+    public Letterhead(String imagePath)
+            throws IOException, IllegalArgumentException {
+        File file = new File(imagePath);
+        requirePNGorJPEG(file);
+        content = ImageIO.read(file);
     }
 
     public BufferedImage getContent() {
         return content;
+    }
+
+    private void requirePNGorJPEG(File file) throws IOException {
+        Path path = file.toPath();
+        String contentType = Files.probeContentType(path);
+        boolean isPNG = Objects.equals(contentType, "image/png");
+        boolean isJPEG = Objects.equals(contentType, "image/jpeg");
+        if (!isPNG && !isJPEG) {
+            throw new IllegalArgumentException(contentType + " is not supported");
+        }
     }
 }
--- src/test/java/jerp/LetterheadTest.java
+++ src/test/java/jerp/LetterheadTest.java
@@ -12,10 +12,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class LetterheadTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"src/test/resources/mockLetterheads/lettehead_coffee.png",
-                            "src/test/resources/mockLetterheads/letterhead_autotrac.svg",
-                            "src/test/resources/mockLetterheads/letterhead_wood.jpg"})
+    @ValueSource(strings = {
+            "src/test/resources/mockLetterheads/lettehead_coffee.png",
+            "src/test/resources/mockLetterheads/letterhead_wood.jpg"
+    })
     void mustNotThrowException(String path) {
         assertDoesNotThrow(() -> {new Letterhead(path);});
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "src/test/resources/mockLetterheads/letterhead_svg.svg",
+            "src/test/resources/mockLetterheads/letterhead_svg2.svg",
+            "src/test/resources/wrongInputFiles/tryPDFasInput.pdf"
+    })
+    void mustThrowIllegalArgumentException(String path) {
+        assertThrows(IllegalArgumentException.class, () -> {new Letterhead(path);});
     }
 }
