512dc9bc429d0c865981b89f78b24d62b7704280
Julien <zigan.julien@gmail.com>
2025-01-15 19:29:39 +0100

    add cleanup class in tests to delete all test pdf files

---
+++ src/test/java/jerp/VisualTestCleanUp.java
@@ -0,0 +1,18 @@
+package jerp;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Objects;
+
+class VisualTestCleanUp {
+    @Test
+    void cleanUp() {
+        File dir = new File("src/test/resources/tmpTestOutput");
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            file.delete();
+        }
+    }
+
+
+}
