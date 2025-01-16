4d00944f2a5e23ee2f3c7e6a631b3e3f30a0c5cb
Julien <zigan.julien@gmail.com>
2025-01-16 14:20:19 +0100

    delete blank lines

--- src/test/java/jerp/BusinessLetterDIN5008Test.java
+++ src/test/java/jerp/BusinessLetterDIN5008Test.java
@@ -1,32 +1,15 @@
 package jerp;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.awt.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BusinessLetterDIN5008Test {
-//    ///    Opens File in PDFReader, leave commented out during regular tests
-//    @Test // Opens the PDFFile on the host system for visual proof
-//    void shouldReturnBlankDINA4PdfFile() throws Exception {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd_H.mm");
-//        String dateExtension = LocalDateTime.now().format(formatter);
-//        String path = "src\\test\\resources\\tmpTestOutput\\TestSaveAsPDF_";
-//        String fqn = path + dateExtension;
-//
-//        BusinessLetterDIN5008 letter = new BusinessLetterDIN5008();
-//        File shouldBeBlankA4Pdf = letter.saveAsPDF(fqn);
-//
-//        Desktop.getDesktop().open(shouldBeBlankA4Pdf);
-//    }
 
     @ParameterizedTest
     @ValueSource(strings = {"0.pdf", "ABD.PDF", "Äüß.pDF", "pDFx.o.z.pdf", "pupe.pdf"})
